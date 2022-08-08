c50d7e7144189d020531e97269d1fdfc13b7c6e1
AbdulLateef1158 <78403686+AbdulLateef1158@users.noreply.github.com>
2022-08-08 22:00:00 +0530

    Add files via upload

---
+++ ThreadExtend.java
@@ -0,0 +1,10 @@
+public class ThreadExtend extends Thread{
+	public void run(){
+		System.out.println("Name of the thread is: "+Thread.currentThread().getName());
+	}
+	public static void main(String args[]){
+		ThreadExtend t=new ThreadExtend();
+		t.setName("Abd");
+		t.start();
+	}
+}
